c6b59be886e5182caa18ede7b69e2007f3f2e01c
Sarath O S <sarathos584@gmail.com>
2023-06-09 17:33:25 +0530

    commited just after adding # to anchor tags to avoid refreshing page

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ $(document).ready(function () {
   $("#toggleBtn").click(function () {
     if ($(this).data("flag")) {
       $(".list-items").addClass("responsive-nav");
-      $('#hamburger').attr("src","./assets/xmark-icon.svg")
+      $("#hamburger").attr("src", "./assets/xmark-icon.svg");
       $(this).data("flag", false);
     } else {
       $(".list-items").removeClass("responsive-nav");
-      $('#hamburger').attr("src","./assets/bar-icon.svg")
+      $("#hamburger").attr("src", "./assets/bar-icon.svg");
       $(this).data("flag", true);
     }
   });
